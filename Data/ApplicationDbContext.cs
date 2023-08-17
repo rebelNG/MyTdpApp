69d2f39979a88c29df4a7a2f7c5a624fd2a50873
Emmanuel Alozieuwa <emmanuelalozieuwa@gmail.com>
2023-08-17 10:20:25 +0100

    Webapp built, Other config to follow

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MyTdpApp.Models;
+
+namespace MyTdpApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MyTdpApp.Models.Blog> Blog { get; set; }
+    }
+}
---
+++ Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyTdpApp.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string Topic { get; set; }
+        public string BlogPost { get; set; }
+
+        public Blog()
+        {
+
+        }
+    }
+}
